2512f10ca15ca20c88a3fbc6c475b68dade2a94c
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-03-05 20:41:10 -0300

    Add code to write users follow action to the log file. Update logs.txt.

--- php/followUser.php
+++ php/followUser.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
     $server = "localhost";
     $username = "root";
     $password = "";
@@ -10,6 +11,17 @@
     $query = "INSERT INTO followers (followed_id, follower_id, following_since) VALUES ('$followed', '$follower', '$date');";
     if($followUserConn->query($query)){
         echo "User followed.";
+        $logFile = fopen("../logs/log.txt", "a") or die("Unable to load log file.");
+        if(isset($_SERVER["HTTP_X_FORWARDED_FOR"]) && $_server["HTTP_X_FORWARDED_FOR"] != ""){
+            $ipAddr = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        }
+        else{
+            $ipAddr = $_SERVER["REMOTE_ADDR"];
+        }
+        $log = "Logged Action: Followed a user.\n" . "Username: $_SESSION[username], ID: $_SESSION[id], IP address: $ipAddr \n" .
+        "The user $_SESSION[username] followed the user id $followed. \n -------- \n";
+        fwrite($logFile, $log);
+        fclose($logFile);  
     }
     else{
         echo "User not followed";
