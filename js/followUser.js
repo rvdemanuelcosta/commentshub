2551907fa338e9841a5cdffccff7835d2cf4f8cc
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-03-05 20:00:45 -0300

    Update logs.

--- js/followUser.js
+++ js/followUser.js
@@ -6,9 +6,21 @@ function followUser(follower){
     xmlhttpFollow.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             console.log("user followed.");
+            document.getElementById("followButton").innerHTML = "Unfollow";
         }
     };
 
     xmlhttpFollow.open("GET", reque, true);
     xmlhttpFollow.send();
+}
+
+function loginToFollow(){
+    alert("You have to be logged in to follow users.");
+}
+
+function unfollow(){
+
+    // code to unfollow bellow
+    alert("unfollowed");
+    console.log("unfollowing")
 }
