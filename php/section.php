a8cde31565b901be592ac5769ba2576415a60150
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-03-13 06:40:28 -0300

    Remove unused commented code.

--- php/section.php
+++ php/section.php
@@ -3,18 +3,7 @@
             session_start();
     }
     $user_authenticated = false;
-    //$user_cookie = null;
-    /*if(isset($_COOKIE['username'])){
-        $user_cookie = $_COOKIE['username'];
-    } */
     if(isset($_SESSION['id'])){
         $user_pfp = "http://$_SERVER[HTTP_HOST]/commentshub/users/$_SESSION[username]/pfp.png";
-        /*
-    if(substr($_SERVER['REQUEST_URI'], -9) == "index.php"){
-        $user_pfp = "users/$_SESSION[username]/pfp";
-    }
-    else{
-        $user_pfp = "../users/" . $_SESSION['username'] . "/pfp";
-    } */
 }
 ?>
