ad5ecc829b274e52d4131d6bd3bf288ac6b77bd2
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-03-13 12:28:51 -0300

    Add responsivity to the header for resolutions with width lower than 320px.

--- php/header.php
+++ php/header.php
@@ -6,17 +6,17 @@ echo "
         <input type='text'> <input type='button' value='&#128270;'>
         <link rel='stylesheet' href='$server_path/css/user_profile.css'>
     </div> 
-        <div class='header-links'>";
-            if(!isset($_SESSION['id'])){
-                echo "<a href='$server_path/login.html'>Login</a>
-                <a href='$server_path/signup.html'>Sign up</a>";
-                }
-                else 
-                {
-                    echo "<a href='$server_path/home.php'>home</a>";
-                    echo "<img src='$user_pfp' id='user_pfp'><a href=" . $server_path . "/php/profile.php" . " class='fit_text'>" . $_SESSION['username'] ."</a>
-                    <a href='" . $server_path . "/php/logout.php'>Log out</a>";
-                }
-                echo "<a href='#'>About us</a>
+    <div class='header-links'>";
+        if(!isset($_SESSION['id'])){
+            echo "<a href='$server_path/login.html'>Login</a>
+            <a href='$server_path/signup.html'>Sign up</a>";
+            }
+            else 
+            {
+                echo "<a href='$server_path/home.php'>home</a>";
+                echo "<img src='$user_pfp' id='user_pfp'><a href=" . $server_path . "/php/profile.php" . " class='fit_text'>" . $_SESSION['username'] ."</a>
+                <a href='" . $server_path . "/php/logout.php'>Log out</a>";
+            }
+            echo "<a href='#'>About us</a>
     </div>";
     ?>
