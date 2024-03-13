7feec5b7c0533be1fd5d1442a9025016d097cfd8
rvdemanuelcosta <emanuelcostalecm@gmail.com>
2024-03-13 06:37:44 -0300

    Remove commented js line of code.

--- php/unfollowUser.php
+++ php/unfollowUser.php
@@ -8,8 +8,6 @@
     $con = new mysqli($server, $dbUser, $dbPass, $db);
     $query = "DELETE FROM followers WHERE followed_id='$followed' AND follower_id='$follower';";
     if($con->query($query)){
-        // TO DELETE, JAVASCRIPT DONT WORK INSIDE A PHP FILE UNLESS IF IT BE INSIDE OF HTML ELEMENTS WITH THE SCRIPT TAG.
-       //alert("user is not following");
        echo "user unfollowed.";
     }
     $con->close();
